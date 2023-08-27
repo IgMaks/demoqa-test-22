a637e3b7b2165b16e314fcd5a2245442f3fed77b
i.maksimov <bif11@mail.ru>
2023-08-27 19:15:59 +0300

    Added testTextBox

---
+++ src/test/java/TxtBoxTests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TxtBoxTests {
+    @Test
+        void fillFormtest(){
+        Configuration.browser = "Chrome";
+        open("https://demoqa.com/text-box");
+
+        $("#userName").setValue("Ivan");
+        $("#userEmail").setValue("Ivan@ya.ru");
+        $("#currentAddress").setValue("Address info");
+        $("#permanentAddress").setValue("Permanent Address- address");
+        $("#submit").click();
+        $("#output").shouldBe(visible);
+
+    }
+}
