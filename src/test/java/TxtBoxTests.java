0996393972409b8bdd63d525aee372067816e9e0
i.maksimov <bif11@mail.ru>
2023-08-27 20:17:37 +0300

    Added testTextBox убрал ошибку

--- src/test/java/TxtBoxTests.java
+++ src/test/java/TxtBoxTests.java
@@ -2,14 +2,14 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TxtBoxTests {
     @Test
-        void fillFormtest(){
-        Configuration.browser = "Chrome";
+    void fillFormTest() {
         open("https://demoqa.com/text-box");
 
         $("#userName").setValue("Ivan");
@@ -18,6 +18,5 @@ public class TxtBoxTests {
         $("#permanentAddress").setValue("Permanent Address- address");
         $("#submit").click();
         $("#output").shouldBe(visible);
-
     }
 }
